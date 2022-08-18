fd7a34580ca36918678a48799af1039a59f7b81e
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-18 09:46:30 +0200

    Got the client to work

--- src/index.php
+++ src/index.php
@@ -2,40 +2,37 @@
 
 require_once '../vendor/autoload.php';
 
-$server   = '192.168.80.2';
-$port     = 8883;
+//$server   = '192.168.80.2';
+$server		= '192.168.95.115';
+$port		= 8883;
 $clientId = 'infoscreen';
+
+echo $server."\n";
+echo $port."\n";
+echo $clientId."\n";
 
 $mqtt = new \PhpMqtt\Client\MqttClient($server, $port, $clientId);
 $connectionSettings = (new \PhpMqtt\Client\ConnectionSettings)
-    ->setUsername('infoscreen')
-    ->setPassword('5k1nnyL4773')
-    ->setUseTls(true)
-    ->setTlsSelfSignedAllowed(true)
-    ->setTlsCertificateAuthorityFile('../certs/192.168.95.115/ca-root-cert.crt')
-    ->setTlsClientCertificateFile('../certs/192.168.80.2_new/client.crt')
-    ->setTlsClientCertificateKeyFile('../certs/192.168.80.2_new/client.key');
+	->setUsername('infoscreen')
+	->setPassword('5k1nnyL4773')
+	->setUseTls(true)
+	->setTlsSelfSignedAllowed(true)
+	->setTlsCertificateAuthorityFile('../certs/192.168.95.115/ca-root-cert.crt')
+	->setTlsClientCertificateFile('../certs/192.168.80.2_new/client.crt')
+	->setTlsClientCertificateKeyFile('../certs/192.168.80.2_new/client.key');
 
 try {
 	$mqtt->connect($connectionSettings, true);
-} catch (\PhpMqtt\Client\Exceptions\ConfigurationInvalidException $e) {
-	echo $e;
-	exit();
-} catch (\PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException $e) {
+} catch (\PhpMqtt\Client\Exceptions\ConfigurationInvalidException|\PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException $e) {
 	echo $e;
 	exit();
 }
 
-$topic = '/hospotal/';
-
 try {
-	$mqtt->subscribe('php-mqtt/client/test', function ($topic, $message) {
+	$mqtt->subscribe('/hospital/', function ($topic, $message) {
 		echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
 	}, 0);
-} catch (\PhpMqtt\Client\Exceptions\DataTransferException $e) {
-	echo $e;
-	exit();
-} catch (\PhpMqtt\Client\Exceptions\RepositoryException $e) {
+} catch (\PhpMqtt\Client\Exceptions\DataTransferException|\PhpMqtt\Client\Exceptions\RepositoryException $e) {
 	echo $e;
 	exit();
 }
@@ -43,16 +40,7 @@ try {
 
 try {
 	$mqtt->loop(true);
-} catch (\PhpMqtt\Client\Exceptions\DataTransferException $e) {
-	echo $e;
-	exit();
-} catch (\PhpMqtt\Client\Exceptions\InvalidMessageException $e) {
-	echo $e;
-	exit();
-} catch (\PhpMqtt\Client\Exceptions\ProtocolViolationException $e) {
-	echo $e;
-	exit();
-} catch (\PhpMqtt\Client\Exceptions\MqttClientException $e) {
+} catch (\PhpMqtt\Client\Exceptions\DataTransferException|\PhpMqtt\Client\Exceptions\InvalidMessageException|\PhpMqtt\Client\Exceptions\ProtocolViolationException|\PhpMqtt\Client\Exceptions\MqttClientException $e) {
 	echo $e;
 	exit();
 }
