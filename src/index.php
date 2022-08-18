f14d196f53819514438379df6bc427612f7b1e1d
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-18 08:52:55 +0200

    asdf

--- src/index.php
+++ src/index.php
@@ -12,15 +12,56 @@ $connectionSettings = (new \PhpMqtt\Client\ConnectionSettings)
     ->setPassword('5k1nnyL4773')
     ->setUseTls(true)
     ->setTlsSelfSignedAllowed(true)
-    ->setTlsCertificateAuthorityFile('../certs/192.168.80.2_new/ca-root-cert.crt')
+    ->setTlsCertificateAuthorityFile('../certs/192.168.95.115/ca-root-cert.crt')
     ->setTlsClientCertificateFile('../certs/192.168.80.2_new/client.crt')
     ->setTlsClientCertificateKeyFile('../certs/192.168.80.2_new/client.key');
-$mqtt->connect($connectionSettings, true);
+
+try {
+	$mqtt->connect($connectionSettings, true);
+} catch (\PhpMqtt\Client\Exceptions\ConfigurationInvalidException $e) {
+	echo $e;
+	exit();
+} catch (\PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException $e) {
+	echo $e;
+	exit();
+}
+
 $topic = '/hospotal/';
-$mqtt->subscribe('php-mqtt/client/test', function ($topic, $message) {
-    echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
-}, 0);
-$mqtt->loop(true);
-$mqtt->disconnect();
+
+try {
+	$mqtt->subscribe('php-mqtt/client/test', function ($topic, $message) {
+		echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
+	}, 0);
+} catch (\PhpMqtt\Client\Exceptions\DataTransferException $e) {
+	echo $e;
+	exit();
+} catch (\PhpMqtt\Client\Exceptions\RepositoryException $e) {
+	echo $e;
+	exit();
+}
+
+
+try {
+	$mqtt->loop(true);
+} catch (\PhpMqtt\Client\Exceptions\DataTransferException $e) {
+	echo $e;
+	exit();
+} catch (\PhpMqtt\Client\Exceptions\InvalidMessageException $e) {
+	echo $e;
+	exit();
+} catch (\PhpMqtt\Client\Exceptions\ProtocolViolationException $e) {
+	echo $e;
+	exit();
+} catch (\PhpMqtt\Client\Exceptions\MqttClientException $e) {
+	echo $e;
+	exit();
+}
+
+try {
+	$mqtt->disconnect();
+} catch (\PhpMqtt\Client\Exceptions\DataTransferException $e) {
+	echo $e;
+	exit();
+}
 
 
